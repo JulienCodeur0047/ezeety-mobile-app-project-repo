c071f5a3bdc59b5e8475fea3c333655613224154
julien <julien@mail.com>
2019-07-11 09:11:47 +0300

    first commit

---
+++ app/src/main/java/com/packag/ezeety/ezeety_home.java
@@ -0,0 +1,13 @@
+package com.packag.ezeety;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ezeety_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ezeety_home);
+    }
+}
